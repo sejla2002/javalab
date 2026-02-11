a85e20caaf824b5c3df23c3ab7c963a7477aa5d3
sejla2002 <ssejlasaid@gmail.com>
2026-02-11 10:15:39 +0530

    Add files via upload

---
+++ lowestpriceproduct.java
@@ -0,0 +1,38 @@
+package sejla;
+class Product {
+	int pcode;
+	String pname;
+	double price;
+	
+	Product(int c, String n, double p) {
+		pcode=c;
+		pname=n;
+		price=p;
+	}
+	void display() {
+		System.out.println("Product Code :"+ pcode);
+		System.out.println("Product Name :"+ pname);
+		System.out.println("Price        :"+ price);
+		System.out.println("-------------------------");
+	}
+}
+
+public class lowestpriceproduct {
+    public static void main(String[] args) {
+    	Product p1= new Product(101,"Laptop",55000);
+    	Product p2= new Product(102,"Mobile",18000);
+    	Product p3= new Product(103,"Printer",12000);
+    	Product lowest=p1;
+    	if (p2.price < lowest.price) {
+    		lowest=p2;
+    	}
+    	if (p3.price < lowest.price) {
+    		lowest=p3;
+    	}
+    	System.out.println("Product with Lowest Price:");
+    	System.out.println("==========================");
+    	lowest.display();
+    }
+		//V TODO Auto-generated method stub}
+
+}
